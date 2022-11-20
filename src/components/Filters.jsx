606c4740733dc3a1672bceffc2f1360aa24ca3fa
tzhovtyi <tzhovtyy@mail.ru>
2022-11-21 04:06:41 +1000

    filtration test

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,9 +1,9 @@
-import React, {useState, forwardRef} from 'react'
+import React, {useState, forwardRef, useEffect} from 'react'
 import "../styles/css/Filters.css"
 import DatePicker from "react-datepicker"
 import "react-datepicker/dist/react-datepicker.css";
 
-export default function Filters(showFilters) {
+export default function Filters(props) {
 //styling, ignore it
     let activeLink = document.querySelector("button.active");
     function buttonStyle(e) {
@@ -23,16 +23,21 @@ const ExampleCustomInput = forwardRef(({ value, onClick }, ref) => (
   ));
 
 
-
-
-
+  //filtration
+function setFilters(name, newValue) {
+    props.setFiltrationParams(prevState => {
+        let obj = prevState;
+        obj[name] = newValue;
+        return obj;
+     })
+    }
 
   return (
-    <div className={showFilters.open? 'filters-main-mobile': `filters-main`}>
-        {showFilters.open? '' : <span className='filters-title'>Filters</span>}
+    <div className={props.open? 'filters-main-mobile': `filters-main`}>
+        {props.open? '' : <span className='filters-title'>Filters</span>}
         <form className='filters-form'>
             <label className='filters-label' htmlFor="from">From</label>
-            <select className='filters-field filters-input' id="from">
+            <select onChange={(e)=> setFilters(e.target.id, e.target.value)} className='filters-field filters-input' id="from">
                 <option>Almaty</option>  
                 <option>Antalya</option>
                 <option>Bali</option>
@@ -58,7 +63,7 @@ const ExampleCustomInput = forwardRef(({ value, onClick }, ref) => (
             </select>
 
             <label className='filters-label' htmlFor="to">To</label>
-            <select className='filters-field filters-input' id="to">
+            <select onChange={(e)=> setFilters(e.target.id, e.target.value)} className='filters-field filters-input' id="to">
                 <option>Almaty</option>  
                 <option>Antalya</option>
                 <option>Bali</option>
@@ -84,12 +89,10 @@ const ExampleCustomInput = forwardRef(({ value, onClick }, ref) => (
             </select>
 
             <label className='filters-label' htmlFor="departure">Departure date</label>
-            <DatePicker selected={fromDate} onChange={(date) => setFromDate(date)} customInput={<ExampleCustomInput />}/>
-
-
+            <DatePicker id='dateFrom' selected={fromDate} value={fromDate} onChange={(date) => {setFromDate(date); setFilters('dateFrom', date)}} customInput={<ExampleCustomInput />}/>
 
             <label className='filters-label' htmlFor="arrival">Arrival date</label>
-            <DatePicker selected={toDate} onChange={(date) => setToDate(date)} customInput={<ExampleCustomInput />} />
+            <DatePicker selected={toDate} onChange={(date) => {setToDate(date); setFilters('dateTo', date)}} customInput={<ExampleCustomInput />} />
 
             <label className='filters-label' htmlFor="weight">Size of delivarable</label>
             <select className='filters-field' id="weight">
@@ -100,11 +103,11 @@ const ExampleCustomInput = forwardRef(({ value, onClick }, ref) => (
 
             <label className='filters-label' for="reward">Reward</label>
             <div className='reward-choice-container'>
-                <button onClick={(e)=>buttonStyle(e)} className='reward-filter-btn reward-filter-btn-left '>Yes</button>
-                <button onClick={(e)=>buttonStyle(e)} className='reward-filter-btn active'>No</button>
-                <button onClick={(e)=>buttonStyle(e)} className='reward-filter-btn reward-filter-btn-right' >N/A</button>
+                <button onClick={(e)=>{buttonStyle(e); setFilters('isRewardable', true)}} className='reward-filter-btn reward-filter-btn-left '>Yes</button>
+                <button onClick={(e)=>{buttonStyle(e); setFilters('isRewardable', false)}} className='reward-filter-btn active'>No</button>
+                <button onClick={(e)=>{buttonStyle(e); setFilters('isRewardable', null)}} className='reward-filter-btn reward-filter-btn-right' >N/A</button>
             </div>
         </form>
     </div>
   )
-}
+  }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -23,7 +23,7 @@ export default function Home() {
         }
     }, [hash, navigate])
 
-    const [requests, setRequests] = useState([]);
+    const [requests, setRequests] = useState([{"id":2,"chatId":"-1001738437352","messageId":"211","from":"St.Petersburg","to":"Denpasar","dateFrom":"2022-11-17","dateTo":"2022-11-24","message":"documents","context":null,"link":null,"isRewardable":true},{"id":3,"chatId":"-1001738437352","messageId":"221","from":"Bali","to":"Moscow","dateFrom":"2022-11-16","dateTo":"2022-12-16","message":"simcard","context":null,"link":null,"isRewardable":true},{"id":4,"chatId":"-1001738437352","messageId":"234","from":"Bali","to":"Moscow","dateFrom":"2022-11-16","dateTo":"2022-11-16","message":"simcard","context":null,"link":null,"isRewardable":true},{"id":5,"chatId":"-1001738437352","messageId":"236","from":"Bali","to":"Moscow, St.Petersburg","dateFrom":"2022-11-16","dateTo":"2022-12-16","message":"simcard","context":"Кто-нибудь летит с Бали в Москву или Питер? надо передать симкарту за вознаграждение! Пишите пожалуйста в лс ❤️","link":null,"isRewardable":true}]);
 
     useEffect(() => {
         fetch(process.env.REACT_APP_API_PREFIX + "/api/requests")
@@ -47,21 +47,45 @@ export default function Home() {
 
     const [showFilters, setShowFilters] = useState(false);
 
+    const [filtrationParams, setFiltrationParams] = useState(
+        {
+            'from': null,
+            'to': null,
+            'dateFrom': null,
+            'dateTo': null,
+            "isRewardable": null,
+        });
+
+        console.log(filtrationParams);
+        
     const openFilters = () => {
         setShowFilters(prev => !prev);
-        console.log(showFilters);
     }
 
-    function filtrate() {
-        
-    }
+    useEffect(()=> {
+        console.log('and i do work')
+    }, [filtrationParams])
+
+    useEffect(() => {
+        console.log('i ran')
+        let allArray = requests;
+        let fil = filtrationParams;
+        let newArray = allArray.filter(function (el) {
+            return ((el.from == fil.from) || fil.from === null)  &&
+                   ((el.to == fil.to) || fil.to === null) &&
+                   ((el.dateFrom == fil.dateFrom) || fil.dateFrom === null) &&
+                   ((el.dateTo == fil.dateTo) || fil.dateTo === null) &&
+                   ((el.isRewardable == fil.isRewardable) || fil.isRewardable === null);
+          });
+        setCards(newArray)        
+    }, [filtrationParams])
     
 
   return (
     <div className='home-main'>
         <Header openFilters={openFilters} />
         <div className='home-container'>
-            <Filters open = {showFilters}/>
+            <Filters open = {showFilters} setFiltrationParams = {setFiltrationParams}/>
            
            {showFilters ? '' : <div className='cards-container'>
                 {cards.map((request, key) => (
