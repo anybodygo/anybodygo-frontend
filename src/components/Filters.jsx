10d9fb9bc98c38bfed743f5612e5ae929b9b2ea0
tzhovtyi <tzhovtyy@mail.ru>
2023-01-14 19:45:15 +1000

    Add redact button

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -23,19 +23,28 @@ export default function Filters({ setFiltrationParams = f => f,
             obj[name] = newValue;
             return {...obj};
          })
-        }
+    }
     
-    function clearFilter(id, setDatepicker = false) {
+    function clearFilter(id, setDatepicker = null) {
         if (setDatepicker) {
             setDatepicker(null);
         }
         document.querySelector(`#${id}`).value = ''; 
         setFilters(id, null)
     }
+    function clearAllFilters() {
+        if (!(filters.from || filters.to || filters.dateFrom || filters.dateTo)) {
+            return;
+        }
+        clearFilter('from'); 
+        clearFilter('to'); 
+        clearFilter('dateFrom', setFromDate); 
+        clearFilter('dateTo', setToDate)
+    }
     
 return (
     <div className={active? 'filters-main-mobile': `filters-main`}>
-        {active? '' : <span className='filters-title'>Filters</span>}
+        {active? '' : <span className='filters-title'>Фильтры</span>}
         <form className='filters-form'>
 
             <label className='filters-label' htmlFor="from">Откуда</label>
@@ -95,15 +104,14 @@ return (
                 </button>
                 }
             </div>
+            <button onClick={clearAllFilters} type='button' className='clear-filters-btn-desktop'>Сбросить фильтры</button>
 
+            {/* only displayed in mobile */}
             <div className='filters-btn-container'>
-                <button type='button' onClick={()=> {clearFilter('from'); clearFilter('to'); 
-                                                    clearFilter('dateFrom', setFromDate); 
-                                                    clearFilter('dateTo', setToDate)}}
-                    className='clear-filters-btn'>Очистить фильтры
-                </button>
+                <button onClick={clearAllFilters} className='clear-filters-btn'>Сбросить фильтры</button>
                 <button type='button' onClick={openFilters} className='apply-filters-btn'>Показать</button>
             </div> 
+
         </form>
     </div>
   )
